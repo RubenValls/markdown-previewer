d0e6f445dab47abec995d9646bbc68ae133931eb
Rubén Valls <r.vallsaparici@gmail.com>
2023-07-26 13:42:27 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div id='container'>
+      <div id='textarea-container'>
+        <div id='textarea-header'>
+          <img src='https://www.svgrepo.com/show/314005/free-code-camp.svg' />
+          <h2>
+            Editor
+          </h2>
+        </div>
+
+      </div>
+      <div id='previewer-container'>
+        <div id='previewer-header'> 
+          <img src='https://www.svgrepo.com/show/314005/free-code-camp.svg' />
+          <h2>
+            Previewer
+          </h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
